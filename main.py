bcf8b86b33f180f1395db8ae7e79f080605fa499
Nytroid <contactdevanshu2007@gmail.com>
2022-04-29 17:53:47 -0500

    .

---
+++ main.py
@@ -0,0 +1,58 @@
+import cv2
+import time
+import datetime
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')  # Uses the haarcascade mdoels to detect faces
+body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')  # Uses the haarcascade mdoels to detect bodies
+
+detection = False
+detection_stopped_time = None
+timer_started = False
+SECONDS_TO_RECORD_AFTER_DETECTION = 5
+
+frame_size = (int(cap.get(3)), int(cap.get(4)))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('video.mp4', fourcc, 20, frame_size)
+
+while True:
+    _, frame = cap.read() 
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # create a grayscale image of the frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # detect faces in the grayscale image
+    bodies = body_cascade.detectMultiScale(gray, 1.3, 5)  # detect bodies in the grayscale image
+
+    if len(faces) + len(bodies) > 0:
+        if detection:
+            timer_started = False
+        else:
+            detection = True
+            current_time = datetime.datetime.now().strftime("at %H-%M-%S on %m-%d-%Y")
+            out = cv2.VideoWriter(
+                f"{current_time}.mp4", fourcc, 20, frame_size)
+            print("Started Recording!")
+    elif detection:
+        if timer_started:
+            if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                detection = False
+                timer_started = False
+                out.release()
+                print('Stop Recording!')
+        else:
+            timer_started = True
+            detection_stopped_time = time.time()
+
+    if detection:
+        out.write(frame)
+    # for (x, y, width, height) in faces:
+    #     cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 3) 
+
+    cv2.imshow('Camera', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+out.release()
+cap.release()
+cv2.destroyAllWindows()
